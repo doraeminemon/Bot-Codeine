f4af2ee1b6e2a57e1f405aacba0aaafe96e19afd
Cody <>
2021-04-17 17:36:06 +0000

    update Sequelize database (it's currently not working)

--- commands/repo.js
+++ commands/repo.js
@@ -1,24 +1,59 @@
-const Discord = require('discord.js');
+const Discord = require('discord.js')
+const client = new Discord.Client()
+const Sequelize = require('sequelize')
+const Sqlite3 = require('sqlite3')
 
-module.exports = function (msg) {
+const sequelize = new Sequelize('sqlite::memory::')
+
+const Tags = sequelize.define('tags', {
+	name: {
+		type: Sequelize.TEXT,
+		unique: true,
+	},
+	description: Sequelize.TEXT,
+	username: Sequelize.STRING,
+	usage_count: {
+		type: Sequelize.INTEGER,
+		defaultValue: 0,
+		allowNull: false,
+	},
+});
+
+client.once('ready', () => {
+	Tags.sync();
+});
+
+module.exports = async function (msg) {
   	let textToArray = msg.content.split(" ")
   	let textMessage = textToArray.shift()
   	let referencedMessage = msg.reference
   	if ( !msg.reference )
 		return;
-	msg.channel.messages.fetch(referencedMessage.messageID).then( message => {
-		const messageEmbed = new Discord.MessageEmbed()
-			.setColor('#264729')
-			.setTitle('Tôi lói thật với các ông là thế này nhớ!')
-			.setURL('https://design101.co/')
-			.setDescription('**Sân Chơi Giới Trẻ** - điểm đến cho người trẻ về các chủ đề đương đại: văn hoá đại chúng; nghệ thuật; tư duy thiết kế; các lĩnh vực sáng tạo; định hướng sự nghiệp; thiết kế xã hội; các ý tưởng cấp tiến... Cộng đồng của chúng tôi bao gồm các chuyên gia, nghiên cứu sinh, sinh viên, nhà thiết kế trong và ngoài nước ở đa dạng lĩnh vực.')
-			.addFields(
-				{ name: 'Link', value:  "https://discord.com/channels/" + referencedMessage.guildID + "/" + referencedMessage.channelID + "/" + referencedMessage.messageID},
-				{ name: 'Nội dung', value: message.content },
-			)
-			.setTimestamp()
+	msg.channel.messages.fetch(referencedMessage.messageID).then( async message => {
+        const linkToPost = "https://discord.com/channels/" + referencedMessage.guildID + "/" + referencedMessage.channelID + "/" + referencedMessage.messageID
+        const textInPost = message.content
 
-		msg.channel.send(messageEmbed);
+        const splitArgs = textInPost.split(' ');
+        const tagName = splitArgs.shift();
+        const tagDescription = splitArgs.join(' ');
+
+        try {
+            const tag = await Tags.create({
+                name: tagName,
+                description: tagDescription,
+                username: message.author.username,
+            });
+            return message.reply(`Tag ${tag.name} added.`);
+        }
+
+        catch (e) {
+            if (e.name === 'SequelizeUniqueConstraintError') {
+                return message.reply('That tag already exists.');
+            }
+            return message.reply('Something went wrong with adding a tag.');
+        }
+
+		msg.channel.send(linkToPost + ";" + textInPost);
 	} );
   // msg.channel.send("**link**\n" + "https://discord.com/channels/" + referencedMessage.guildID + "/" + referencedMessage.channelID + "/" + referencedMessage.messageID);
 
