248467b591293e487c3be36dc7b696468433f5db
Cody <>
2021-04-28 16:29:47 +0000

    push it to the limit

--- commands/repo.js
+++ commands/repo.js
@@ -33,47 +33,45 @@ module.exports = async function(msg) {
         function onlyNumbers(element, validationArray) {
             return validationArray.includes(element-1)
         }
-
-        const filter = m => m.content.split(' ').map(Number).every(e => onlyNumbers(e, tagIndexing))
-
-        const collector = msg.channel.createMessageCollector(filter, { max: 1, time: 5000 });
-
+        const filter = m => m.content.split(/[^\d]+/).map(Number).every(e => onlyNumbers(e, tagIndexing))
+        const collector = msg.channel.createMessageCollector(filter, { time: 60000 });
         collector.on('collect', m => {
-            console.log(`Collected ${m.content}`)
-            collector.stop()
+            console.log(`Collected ${m}`)
+            if (m.size !== 0) {
+                collector.stop()
+            }
         });
-
         collector.on('end', collected => {
             if (collected.size === 0) {
                 msg.channel.send('Ơ, địt mẹ không gắn tag à')
             } else {
-                const processedInput = [...new Set(collected.first().content.split(' ').map(Number))].sort((a, b) => a - b)
+                const processedInput = [...new Set(collected.first().content.split(/[^\d]+/).map(Number))].sort((a, b) => a - b)
 
                 const getTagName = []
-                processedInput.forEach(e => {
-                    getTagName.push(getTagList[e-1].tagName)
-                })
-
-                msg.channel.send(getTagName.join(', '));
+                try {
+                    processedInput.forEach(e => {
+                        getTagName.push(getTagList[e-1].tagName)
+                    })
+                    msg.channel.send(getTagName.join(', '));
+                    return
+                } catch (error) {
+                    msg.channel.send('loi roi')
+                    console.log(error)
+                }
             }
         });
     }
 
     msg.channel.messages.fetch(referencedMessage.messageID).then(async post => {
-
         let postURL = 'https://discord.com/channels/' + referencedMessage.guildID + '/' + referencedMessage.channelID + '/' + referencedMessage.messageID;
         let textInPost = post.content
-
         const repoTitle = titleInput.trim()
         const repoContent = textInPost
         const repoAuthor = msg.author.tag
         const repoURL = postURL
         const attachmentArray = []
-        post.attachments.forEach(attachment => {
-            attachmentArray.push(attachment.url)
-        })
+        post.attachments.forEach(attachment => attachmentArray.push(attachment.url))
         const repoAttachments = attachmentArray.join(', ');
-
             const [repo, created] = await connection.model('database').findOrCreate({
                 where: {
                     [Op.or]: [{title: repoTitle}, {url: repoURL}]
@@ -84,13 +82,10 @@ module.exports = async function(msg) {
                     url: repoURL,
                     author: repoAuthor,
                     attachments: repoAttachments,
-                    // tags: repoTags
                 }
             });
-
             if (created) {
                 findTags(repoTitle)
-                
             } else {
                 msg.reply(`Có cái này mất rồi.`)
             }
